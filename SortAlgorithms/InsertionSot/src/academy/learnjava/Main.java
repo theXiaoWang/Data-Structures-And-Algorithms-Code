6b731ce973532cb04ade7f5a2d2537ff29e835dd
Xiao Wang <ooloogwangxiao@gmail.com>
2021-07-20 19:07:24 +0100

    Add recursion way for insertion sort

--- SortAlgorithms/InsertionSot/src/academy/learnjava/Main.java
+++ SortAlgorithms/InsertionSot/src/academy/learnjava/Main.java
@@ -8,6 +8,14 @@ public class Main {
 	// write your code here
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
 
+//        insertionSort(arr);
+        recursionInsertionSort(arr, arr.length);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //Using While loop
+    private static void insertionSort(int[] arr) {
         for (int firstUnsortedIndex = 1; firstUnsortedIndex < arr.length; firstUnsortedIndex++) {
 
             int newElement = arr[firstUnsortedIndex];
@@ -21,17 +29,35 @@ public class Main {
             }
             arr[i] = newElement; //i is the insertion place index
         }
+    }
 
-//        for (int candidateIndex = 1; candidateIndex < arr.length; candidateIndex++) {
-//            int insertPlaceIndex = candidateIndex;
-//            int temp = arr[candidateIndex];
-//            for (int i = candidateIndex - 1; i >= 0; i--) {
-//                if (temp < arr[i]) {
-//                    arr[insertPlaceIndex--] = arr[i];
-//                }
-//            }
-//            arr[insertPlaceIndex] = temp;
-//        }
-        System.out.println(Arrays.toString(arr));
+    //Using for loop
+    private static void insertionSort2(int[] arr) {
+        for (int candidateIndex = 1; candidateIndex < arr.length; candidateIndex++) {
+            int insertPlaceIndex = candidateIndex;
+            int temp = arr[candidateIndex];
+            for (int i = candidateIndex - 1; i >= 0; i--) {
+                if (temp < arr[i]) {
+                    arr[insertPlaceIndex--] = arr[i];
+                }
+            }
+            arr[insertPlaceIndex] = temp;
+        }
+    }
+
+    //Using recursion
+    private static void recursionInsertionSort(int[] arr, int items) {
+        if (items < 2) {
+            return;
+        }
+        recursionInsertionSort(arr, items - 1);
+        int i = items - 1;
+        int newElement = arr[i];
+
+        while (i > 0 && arr[i - 1] > newElement) {
+            arr[i] = arr[i - 1];
+            i--;
+        }
+        arr[i] = newElement;
     }
 }
